e1565ebbf0f58df20d644fc508d9920d0c9f3774
tywi <tyletcole@gmail.com>
2018-11-03 22:03:32 -0600

    made canvas for intro page animation

--- project-3/client/src/pages/Intro/Intro.js
+++ project-3/client/src/pages/Intro/Intro.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as d3 from "d3";
 import "./Intro.css";
 
 class Intro extends Component {
 
     render() {
-        return(
+        return (
             <div className="div">
-                <button className="button">
+                {/* <button className="button">
                     <Link className="link" to={"/main"}>(click)</Link>
                 </button>
-            </div>     
+                <pre>
+                    <canvas id="canvas"/>
+                </pre> */}
+            </div>
         )
+    }
+
+    componentDidMount() {
+        this.animation();
+    };
+
+    animation() {
+
+        //Creating the canvas
+        const width = 500;
+        const height = 500;
+        
+        const canvas = d3.select("div").append("canvas")
+            .attr("id", "canvas")
+            .attr("width", `${width}px`)
+            .attr("height", `${height}px`)
+
+        canvas.node().getContext("2d");
+
+        //Kicking off the animation
+        //
+        //
     }
 
 }
